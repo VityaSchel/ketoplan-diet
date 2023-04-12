d44cd7a4c120934359cf8397f4e6988d60a69e47
hloth <gitlab@hloth.dev>
2023-04-12 14:21:44 +0400

    Mobile styles

--- src/widgets/plan-result/ui/timer/index.tsx
+++ src/widgets/plan-result/ui/timer/index.tsx
@@ -1,13 +1,15 @@
 import styles from './styles.module.scss'
 import TimerClock from '@/entities/plan-result/timer-clock'
+import GetPlanModal from '@/features/get-plan-modal'
 
 export default function Timer() {
   return (
     <div className={styles.timer}>
       <div className={styles.left}>
         <TimerClock />
-        <span>Бесплатный план доступен ограниченное время</span>
+        <span>План доступен ограниченное время</span>
       </div>
+      <GetPlanModal>Получить план</GetPlanModal>
     </div>
   )
 }
