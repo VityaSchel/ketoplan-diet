bb7cf26741299f8cc54fc8aece55a4f1ad553b6a
hloth <gitlab@hloth.dev>
2023-04-19 16:10:26 +0400

    Loading

--- src/features/loading-results/index.tsx
+++ src/features/loading-results/index.tsx
@@ -1,15 +1,29 @@
+import React from 'react'
 import styles from './styles.module.scss'
 import cx from 'classnames'
 
 export default function LoadingResults({ open }: {
   open: boolean
 }) {
-  const percentage = 30
+  const [percentage, setPercentage] = React.useState(0)
+
+  React.useEffect(() => {
+    if (open) {
+      const timeout = setTimeout(() => {
+        setPercentage(Math.min(99, percentage + 1))
+      }, 10)
+      return () => clearTimeout(timeout)
+    }
+  }, [open, percentage, setPercentage])
+
+  React.useEffect(() => {
+    if(!open) setPercentage(0)
+  }, [open])
 
   return (
     <div className={cx(styles.loading, { [styles.visible]: open })}>
-      <div className={styles.circle}>
-        <div className={cx(styles.arc, { [styles.halfThrough]: percentage >= 50 })} style={{ '--arc-percentage': `${percentage >= 50 ? 100 - percentage : percentage}deg` }} />
+      <div className={cx(styles.circle, { [styles.halfThrough]: percentage >= 50 })}>
+        <div className={styles.arc} style={{ '--arc-percentage': `${percentage >= 50 ? 100 - percentage : percentage}deg` }} />
         <div className={styles.text}>{percentage}%</div>
       </div>
       <span className={styles.label}>Анализируем данные</span>
