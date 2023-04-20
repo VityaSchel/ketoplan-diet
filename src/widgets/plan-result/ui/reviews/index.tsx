63eb39f7b60d4f327bb80cf4468e1b957af6208b
hloth <gitlab@hloth.dev>
2023-04-20 14:55:51 +0400

    replace second review

--- src/widgets/plan-result/ui/reviews/index.tsx
+++ src/widgets/plan-result/ui/reviews/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from './styles.module.scss'
 import { ReviewCase as Case } from '@/entities/plan-result/reviews/case'
 import { ReviewInfo as Info } from '@/entities/plan-result/reviews/info'
@@ -5,31 +6,37 @@ import case1_before from '@/assets/reviews/case1_before.png'
 import case1_after from '@/assets/reviews/case1_after.png'
 import case2_before from '@/assets/reviews/case2_before.png'
 import case2_after from '@/assets/reviews/case2_after.png'
+import { ResumeContext } from '@/shared/context/resume-context'
 
 export default function Reviews() {
+  const result = React.useContext(ResumeContext)
+
   return (
     <div className={styles.reviewsContainer}>
       <div className={styles.reviews}>
         <Info />
         <div className={styles.cases}>
-          <Case
-            name='Мария'
-            age={28}
-            story={`Очень эффективное питание! Наконец-то нашла индивидуально разработанный план по кето питанию, ведь данная система питания очень эффективна! Самый быстрый, простой и вкусный способ сбросить лишние килограммы и полностью сменить гардероб! Я влезла в свои старые джинсы. Урааааа! Буду рекомендовать план питания подругам, друзьям и родственникам, чтобы закатывать кето ужины и семейные кето посиделки. Всем рекомендую!`}
-            images={{
-              before: case1_before,
-              after: case1_after
-            }}
-          />
-          <Case
-            name='Людмила'
-            age={34}
-            story={`Искала диету, которая подходила бы при диабете. Наткнулся на статью про кето питание, а потом повезло получить индивидуальный план питания. Рецепты разработаны под любой вкус и кошелек, полноценное питание и никакого голодания. Вес начал уходить, уровень сахара в крови падать и я начал жить по-новому! Нисколько не раздумывайте, покупайте кето план и худейте с удовольствием! Здоровье не купишь, а вот индивидуальный план вполне разумная трата!`}
-            images={{
-              before: case2_before,
-              after: case2_after
-            }}
-          />
+          {result.foodType === 'dump' ? (
+            <Case
+              name='Мария'
+              age={28}
+              story={`Очень эффективное питание! Наконец-то нашла индивидуально разработанный план по кето питанию, ведь данная система питания очень эффективна! Самый быстрый, простой и вкусный способ сбросить лишние килограммы и полностью сменить гардероб! Я влезла в свои старые джинсы. Урааааа! Буду рекомендовать план питания подругам, друзьям и родственникам, чтобы закатывать кето ужины и семейные кето посиделки. Всем рекомендую!`}
+              images={{
+                before: case1_before,
+                after: case1_after
+              }}
+            />
+          ) : (
+            <Case
+              name='Виталий'
+              age={34}
+              story={`Искал диету, которая подходила бы при диабете. Наткнулся на статью про кето питание, а потом повезло получить индивидуальный план питания. Рецепты разработаны под любой вкус и кошелек, полноценное питание и никакого голодания. Вес начал уходить, уровень сахара в крови падать и я начал жить по-новому! Нисколько не раздумывайте, покупайте кето план и худейте с удовольствием! Здоровье не купишь, а вот индивидуальный план вполне разумная трата!`}
+              images={{
+                before: case2_before,
+                after: case2_after
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
